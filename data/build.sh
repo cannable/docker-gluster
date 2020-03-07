4d4107d840aadd3bbf317725fb47409565dc2e5d
C. Annable <christopher.annable@gmail.com>
2020-03-07 13:38:01 -0500

    Remove nfs-ganesha bits - they will be moved to a separate container.

--- data/build.sh
+++ data/build.sh
@@ -4,47 +4,24 @@
 # Gluster Build Script
 # ------------------------------------------------------------------------------
 
-GANESHA_MAJOR=2.8
-GANESHA_MINOR=2.8.0
 
-
-# Drop repo files for Gluster and nfs-ganesha
+# Drop repo files for Gluster
 curl -o /etc/yum.repos.d/glusterfs-rhel8.repo https://download.gluster.org/pub/gluster/glusterfs/LATEST/RHEL/glusterfs-rhel8.repo
-
-cat << HERE > /etc/yum.repos.d/nfs-ganesha.repo
-[nfs-ganesha]
-name=nfs-ganesha
-baseurl=https://download.nfs-ganesha.org/${GANESHA_MAJOR}/${GANESHA_MINOR}/RHEL/el-8/\$basearch/
-enabled=1
-gpgcheck=1
-gpgkey=https://download.nfs-ganesha.org/${GANESHA_MAJOR}/rsa.pub
-
-[nfs-ganesha-noarch]
-name=nfs-ganesha-noarch
-baseurl=https://download.nfs-ganesha.org/${GANESHA_MAJOR}/${GANESHA_MINOR}/RHEL/el-8/noarch/
-enabled=1
-gpgcheck=1
-gpgkey=https://download.nfs-ganesha.org/${GANESHA_MAJOR}/rsa.pub
-
-HERE
 
 
 # Install packages
-dnf --setopt=tsflags=nodocs -y check-update
-dnf -y install dnf-plugins-core
+dnf --setopt=tsflags=nodocs -y install dnf-plugins-core
 dnf config-manager --set-enabled PowerTools
-#dnf -y install glusterfs-server nfs-ganesha-gluster
+
 dnf -y install \
   glusterfs-cli \
   glusterfs-server \
-  nfs-ganesha-gluster \
   bind-utils
 
 
 # Enable Services
 ln -s /lib/systemd/system/glusterd.service /etc/systemd/system/multi-user.target.wants/glusterd.service
 
-#Created symlink /etc/systemd/system/multi-user.target.wants/glusterd.service → /usr/lib/systemd/system/glusterd.service.
 
 # Cleanup
 dnf clean all
